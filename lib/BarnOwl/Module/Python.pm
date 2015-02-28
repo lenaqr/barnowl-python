9421b4459adb3336922e9ecaabe1faeecdf28827
Anthony Lu <lua@mit.edu>
2015-02-27 22:47:57 -0500

    Relocate Python code out of the lib directory

--- lib/BarnOwl/Module/Python.pm
+++ lib/BarnOwl/Module/Python.pm
@@ -3,6 +3,6 @@ package BarnOwl::Module::Python;
 use warnings;
 use strict;
 use File::Basename;
-use Inline "Python" => dirname(__FILE__)."/python.py";
+use Inline "Python" => dirname(__FILE__)."/../../../python/main.py";
 
 init();
